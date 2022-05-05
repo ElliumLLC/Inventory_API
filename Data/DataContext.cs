527b046014d9ad3742673ed5f3893e2e2ce00b85
Edgar Martinez Martinez <EXM180001@utdallas.edu>
2022-05-05 02:36:10 -0500

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InventoryAPI;
+
+namespace InventoryAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Inventory> inventory { get; set; }
+    }
+}
